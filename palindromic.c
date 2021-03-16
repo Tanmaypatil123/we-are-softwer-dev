1b2ce35c52fb8c3511a690b038a7fa9c24adaa00
Tanmaypatil123 <77950208+Tanmaypatil123@users.noreply.github.com>
2021-03-16 20:12:57 +0530

    Update palindromic.c

--- palindromic.c
+++ palindromic.c
@@ -13,9 +13,9 @@ int main(int argc, char const *argv[])
         n=n/10;
     }
     if (num==rev)
-    printf("%d is palindromic number",num);
+    printf("%ld is palindromic number",num);
     else
-    printf("%d is not palindromic number",num);
+    printf("%ld is not palindromic number",num);
 
     return 0;
 }
